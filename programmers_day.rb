675329ca9a26b8ab0bd6f67f25de0088e8c6fb49
Ivan Chavero <ichavero@redhat.com>
2018-09-13 16:33:44 -0600

    Fix programmers day Ruby code

---
+++ programmers_day.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+# Fun code for programmers day
+# Programmers day is the 256th day of the year
+# https://en.wikipedia.org/wiki/Day_of_the_Programmer
+# Iván Chavero <ichavero@chavero.com.mx>
+
+
+if Time.now.yday == 256
+   puts "Happy programmers day!!";
+else 
+  puts "Happy shitty normal day!!";
+end
