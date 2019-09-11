33594bb8f25c233ce9902aeb0ac77b20f489e26e
Jorge Luna <seguidor777@gmail.com>
2019-09-11 13:25:28 -0500

    Add Crystal script

---
+++ programmers_day.cr
@@ -0,0 +1,10 @@
+#!/usr/bin/env crystal
+# "Ruby like" language, fast as C
+# Test it on https://play.crystal-lang.org
+# By: Jorge Luna seguidor777@gmail.com
+
+if Time.now.to_s("%j") == "256"
+	puts "Happy programmers day!!"
+else
+	puts "Happy normal day!!"
+end
